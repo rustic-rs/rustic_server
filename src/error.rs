134b6db25eff3049487a7efc7f148a807dfd8013
simonsan <14062932+simonsan@users.noreply.github.com>
2023-09-13 14:34:10 +0200

    refactor II
    Signed-off-by: simonsan <14062932+simonsan@users.noreply.github.com>

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,1 @@
-use axum::http::StatusCode;
-use std::borrow::Cow;
 
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-pub type StatusResult<T> = std::result::Result<T, StatusError<'static>>;
-
-pub struct StatusError<'a> {
-    pub status: StatusCode,
-    pub message: Cow<'a, str>,
-}
